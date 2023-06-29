45f9a4e4251acded010a958b1c8f79b41cfe9958
kopperqu <email@email.com>
2023-06-29 14:45:24 -0500

    Continued implementing the logic for the game. Need to create a "Player" class and maybe a "Card/s" class(es) to help fix complex logic.

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ let doubleDownButton = document.getElementById("doubleDownButton")
 let splitButton = document.getElementById("splitButton")
 let standButton = document.getElementById("standButton")
 let isFirstCards
+let numberOfSplitCards = 0
+let hasSelectedStand
 
 function startGame(){
+    numberOfSplitCards = 0
     isFirstCards = true
     playersCards=[]
     dealerCards=[]
@@ -18,7 +21,12 @@ function startGame(){
     
     renderDealerCards()
     renderPlayerCards()
-    renderCorrectActionsToPlayer()
+    renderCorrectActionsToPlayer()    
+    isFirstCards=false
+}
+
+function splitHandLogic(){
+
 }
 
 
@@ -35,23 +43,33 @@ function renderCorrectActionsToPlayer(){
     playersTotal = countCards(playersCards)
     //Look at players cards it blackjack do nothing player won
     if (playersTotal===21){
-        alert("BLACKJACK")
+        dealerPlays()
     }
     //If player has cards less than totaling less than 21 show hit and stand buttons
-    if (playersTotal<21){
+    if (playersTotal<21 && !hasSelectedStand){
         hitButton.hidden = false
         standButton.hidden = false
     }
     //If player has 2 of same card show split button
-    if ()
+    if (isFirstCards && areCardsTheSame(playersCards)){
+        splitButton.hidden=false
+    }else{
+        splitButton.hidden=true
+    }
+
     //If player was dealt their first 2 cards and it is a 9 10 or 11 show double down button otherwise disable it
     if ([9,10,11].includes(playersTotal) && isFirstCards){
         alert(playersTotal)
         doubleDownButton.hidden = false
-        isFirstCards=false
     }else {
         doubleDownButton.hidden = true
     }
+}
+function areCardsTheSame(cards){
+    if (cards[0] === cards [1]){
+        return true
+    }
+    return false
 }
 
 function renderDealerCards(){
@@ -75,30 +93,62 @@ function dealCard(){
     return card
 }
 
+function playerHits(){
+    hit(playersCards)
+    playersTotal = countCards(playersCards)
+    if (playersTotal>21){
+        alert("BUST")
+        hitButton.hidden=true
+        standButton.hidden=true        
+    }
+}
+
 // Hit: Ask for another card. You can ask for a hit until you decide to stand or else bust
-function hit(){
-    playersCards.push(dealCard())
+function hit(cards){
+    cards.push(dealCard())
     renderPlayerCards()
     renderCorrectActionsToPlayer()
 }
 // Stand: Decide that you take no additional cards. The dealer can then play this hand
 function stand(){
-
+    hasSelectedStand = true
+    hitButton.hidden=true
+    standButton.hidden=true
+    dealerPlays()
 }
 // Double Down: Double the amount of your bet + an extra card + stand
 function doubleDown(){
-
+    hit(playersCards)
+    stand()
 }
 // Split: If you have two cards of the same value, you can split them into two separate hands. The bet is the same as the original bet so essentially doubling your bet.
 function splitCards(){
-
+    numberOfSplitCards += 1
+    splitHandLogic()
 }
 
 function dealerPlays(){
-// The dealer automatically wins if you bust or surrender
-// He also wins if his total hand value is closest to 21
-// If the dealer has a total of 16 or less, he must hit
-// If the dealer has a total of 18 or more, then he must stand
-// If the dealer has a hard 17, he must stand
-// If the dealer has a soft 17, the dealer must hit
+    renderDealerCards()
+    playersTotal = countCards(playersCards)
+    dealersTotal = countCards(dealerCards)
+    let dealerBustorStand = false
+    if (playersTotal>21){
+        alert("dealer wins")
+    }
+    if (dealersTotal>21){
+        dealerBustorStand=true
+    }
+    if (dealersTotal<=16){
+        hit(dealerCards)
+        return
+    }else if (dealersTotal>=18){
+        dealerBustorStand=true
+    }
+    // If the dealer has a hard 17, he must stand
+    // If the dealer has a soft 17, the dealer must hit
+    if ((21-dealersTotal)<(21-playersTotal)){
+        alert("Dealer WINS")
+    }else{
+        alert("you win")
+    }
 }
